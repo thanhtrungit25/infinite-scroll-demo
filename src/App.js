80693fb0a48a0709892b2d05fae55c38df778a11
Trung DG <thanhtrung.it25@gmail.com>
2021-08-09 14:51:50 +0700

    WithContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-import WithStateMachine from './WithStateMachine'
+import WithContext from './WithContext'
 import WithReducer from './WithReducer'
+import WithStateMachine from './WithStateMachine'
 
 function App() {
   return (
     <div className='App'>
       <h1>Data Loading Examples</h1>
+
+      <h2>Data Loading with Context</h2>
+      <WithContext />
 
       <h2>Data Loading with Reducer</h2>
       <WithReducer />
