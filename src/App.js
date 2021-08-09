aab7292320563f387a5a24f53870df7ee5038b5c
Trung DG <thanhtrung.it25@gmail.com>
2021-08-09 14:36:28 +0700

    Add WithReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import WithStateMachine from './WithStateMachine'
+import WithReducer from './WithReducer'
 
 function App() {
   return (
     <div className='App'>
       <h1>Data Loading Examples</h1>
+
+      <h2>Data Loading with Reducer</h2>
+      <WithReducer />
 
       <h2>XState Data Loading Services</h2>
       <WithStateMachine />
