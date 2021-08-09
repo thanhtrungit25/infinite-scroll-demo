c7135ddf06443622a0f4b241db2efddebc8a0bb5
Trung DG <thanhtrung.it25@gmail.com>
2021-08-09 14:11:40 +0700

    Add WithStateMachine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import WithStateMachine from './WithStateMachine'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Data Loading Examples</h1>
+
+      <h2>XState Data Loading Services</h2>
+      <WithStateMachine />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
